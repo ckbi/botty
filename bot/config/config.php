c91d1025735335ff731bfdccc701c711050ac913
akame <87499021+ckbi@users.noreply.github.com>
2023-11-12 21:04:29 +0100

    Create config.php

---
+++ bot/config/config.php
@@ -0,0 +1,21 @@
+<?php
+
+$auchats = array('chat id qui');
+$owner = '1058653930,';
+$bowner = 'balenottere';
+$preusers = array(premiums vanno qui);
+
+if($userId == '1058653930'){
+    $user = '@'.$username.'[God]';
+  $plan = 'God';
+}
+elseif(in_array($userId,$preusers)){
+    $user = '@'.$username.'[𝗣𝗿𝗲𝗺𝗶𝘂𝗺✨]';
+    $plan = 'premium';
+}
+else {
+    $user = '@'.$username.'[𝗙𝗿𝗲𝗲]';
+    $plan = 'free';
+}
+
+?>
